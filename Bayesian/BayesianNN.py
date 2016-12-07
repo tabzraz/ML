a28de424e08818a006d3d994c926d092d74974a0
Tabz <tabzraz@gmail.com>
2016-12-07 17:06:58 +0000

    Partial again

--- Bayesian/BayesianNN.py
+++ Bayesian/BayesianNN.py
@@ -28,6 +28,10 @@ class BayesianFC:
         # todo: change this to be an output of the network?
         self.likelihood_std = 1
 
+        # Weight matrices as variables
+        self.W = tf.Variable(initial_value=np.zeros(shape=(input_dim, output_dim)), dtype=tf.float32)
+        self.b = tf.Variable(initial_value=np.ones(shape=(output_dim)) * 0.1, dtype=tf.float32)
+
     def output(self, input, W=None, b=None, sample=False):
         # If we have not provided the weights, then use our distribution to generate them
         if W is None:
@@ -44,9 +48,9 @@ class BayesianFC:
         return tf.nn.relu(tf.matmul(input, W) + b)
 
     def gaussian_pdf(self, x, mu, sigma):
-        return tf.exp(-((x - mu)**2)/2*(sigma**2))/tf.sqrt(2*np.pi*(sigma**2))
+        return tf.exp(-((x - mu)**2) / 2 * (sigma**2)) / tf.sqrt(2 * np.pi * (sigma**2))
 
-    def update(self, data, N):
+    def calculate_loss(self, data, N):
 
         loss = 0
 
@@ -57,31 +61,40 @@ class BayesianFC:
 
             W_sigmas = tf.nn.softplus(self.qw_p)
             b_sigmas = tf.nn.softplus(self.qb_p)
-            W = self.qw_mean + W_sigmas * epsilon_w
-            b = self.qb_mean + b_sigmas * epsilon_b
+            self.W.assign(self.qw_mean + W_sigmas * epsilon_w)
+            self.b.assign(self.qb_mean + b_sigmas * epsilon_b)
 
             qw_i = tf.reduce_sum(self.gaussian_pdf(W_sigmas * epsilon_w, 0, W_sigmas))
             qw_i += tf.reduce_sum(self.gaussian_pdf(b_sigmas * epsilon_b, 0, b_sigmas))
             log_qw_i = tf.log(qw_i)
 
-            pw_i = tf.reduce_sum(self.gaussian_pdf(W, self.pw_mean, self.pw_sigma))
-            pw_i += tf.reduce_sum(self.gaussian_pdf(b, self.pb_mean, self.pb_sigma))
+            pw_i = tf.reduce_sum(self.gaussian_pdf(self.W, self.pw_mean, self.pw_sigma))
+            pw_i += tf.reduce_sum(self.gaussian_pdf(self.b, self.pb_mean, self.pb_sigma))
             log_pw_i = tf.log(pw_i)
 
             data_likelihood = 0
             for (x, y) in data:
                 # We assume the nn is a probabilistic model P(y|x,w)
-                output = self.output(x, W=W, b=b)
+                output = self.output(x, W=self.W, b=self.b, sample=False)
                 data_likelihood += tf.reduce_sum(self.gaussian_pdf(y, output, self.likelihood_std))
 
             log_data_likelihood = tf.log(data_likelihood)
 
             loss += -log_qw_i - log_pw_i - log_data_likelihood
 
+        weight_variables = [self.W, self.b]
+        variational_mean_variables = [self.qw_mean, self.qb_mean]
+        variational_std_variables = [self.qw_p, self.qb_p]
+        return loss, weight_variables, variational_mean_variables, variational_std_variables
 
+    def update(self, data, N, sess, optimiser):
+        loss, w_vars, v_m_vars, v_std_vars = self.calculate_loss(data, N)
 
+        weight_grads_and_vars = optimiser.compute_gradients(loss, var_list=w_vars)
+        variational_mean_grads_and_vars = optimiser.compute_gradients(loss, var_list=v_m_vars)
+        variational_std_grads_and_vars = optimiser.compute_gradients(loss, var_list=v_std_vars)
 
-
+        new_variational_mean_grads_and_vars = [(gv+gw,v) for (gw,_),(gv,v) in zip(weight_grads_and_vars, variational_mean_variables)]
 
 
 
