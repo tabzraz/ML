6467cfa261fdcba281744f94b4a0dafc77350b85
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-01-18 01:27:13 +0000

    Gradient clipping change

--- Misc/Gradients.py
+++ Misc/Gradients.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
 
+
 def clip_grads(grads_vars, clip):
-    clipped = []
-    for grad, var in grads_vars:
-        if grad is not None:
-            clipped.append((tf.clip_by_norm(grad, clip), var))
-    return clipped
+    # print("Input:\n", grads_vars)
+    grads_vars = list(filter(lambda x: x[0] is not None, grads_vars))
+    # print("\nNones removed:\n", grads_vars)
+    grads = [g for g, v in grads_vars]
+    variables = [v for g, v in grads_vars]
+    # print("\nVariables:\n", variables)
+    clipped, norm = tf.clip_by_global_norm(grads, clip)
+    # print("\nClipped:\n", clipped)
+    # print("\nNorm:\n", norm)
+    clipped_grads = zip(clipped, variables)
+    # print("\nZipped\n:", clipped_grads)
+    return clipped_grads, norm
