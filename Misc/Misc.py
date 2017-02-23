a064ac02ec62e9f4c15f0b7e0ac6abda736269b9
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-02-23 18:49:54 +0000

    Conv size fixes for padding

--- Misc/Misc.py
+++ Misc/Misc.py
@@ -1,4 +1,8 @@
 from math import ceil
 
-def tf_conv_size(W, f, s):
-    return ceil((W - f + 1) / s)
+
+def tf_conv_size(W, f, s, padding="same"):
+    if padding == "same":
+        return ceil(W / s)
+    else:
+        return ceil((W - f + 1) / s)
